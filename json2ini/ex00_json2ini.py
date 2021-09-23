3dd7bc91e42024ee06878c534f9b6ab4471556d9
nahidath <vatoch@hotmail.fr>
2021-09-23 03:56:49 -0400

    python exos

--- json2ini/ex00_json2ini.py
+++ json2ini/ex00_json2ini.py
@@ -1,1 +1,8 @@
-import configparser
+import json
+
+with open("test.json") as json_file:
+    json_data = json.load(json_file)
+
+with open("fichier.ini", 'w') as fw:
+    for key, value in json_data.items():
+        fw.write(key + "=" + str(value) + "\n")
