f962b11c41af7ffb153210244f9d128841ce9196
nahidath <vatoch@hotmail.fr>
2021-09-24 03:06:50 -0400

    exo4

--- hangmanGame/words.py
+++ hangmanGame/words.py
@@ -1,8 +1,3 @@
-#EXERCICE NON TERMINE
-
-
-import random
-
 english_words = ["aback", "abaft", "abandoned", "abashed", "aberrant", "abhorrent", "abiding", "abject", "ablaze",
                  "able", "abnormal", "aboard", "aboriginal", "abortive", "abounding", "abrasive", "abrupt", "absent",
                  "absorbed", "absorbing", "abstracted", "absurd", "abundant", "abusive", "accept", "acceptable",
@@ -16,27 +11,3 @@ english_words = ["aback", "abaft", "abandoned", "abashed", "aberrant", "abhorren
                  "caption", "car", "card", "care", "careful", "careless", "caring", "carpenter", "carriage", "carry",
                  "cars", "cart", "carve", "cast", "cat", "cats", "cattle", "cause", "cautious", "cave", "ceaseless"]
 
-gword = random.choice("english_words")
-size_gword=len(gword)
-i=size_gword+2
-result = ""
-print("Welcome to the Hangman Game !\nA word to guess was chosen, let's start!!!!\nNumbers of chance:",i)
-while i>0:
-    ch_entered=input("Please enter a character:")
-    if ch_entered not in gword:
-       i-=1
-       print("Wrong !!! Try again, please")
-       print("Numbers of chance:",i)
-       ch_entered=input("Please enter a character:")
-    else:
-        print("Yes!! That's it !!")
-        for j in gword:
-            if gword[j] == ch_entered:
-                ind=j
-        result[ind]=ch_entered
-        print(result)
-        ch_entered = input("Please enter a character:")
-if i<0:
-    print("You loose, you can cry...")
-if result == gword:
-    print("You win !! Good game!")
